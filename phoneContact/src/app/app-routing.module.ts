f189fe94fbd7b31753bc3ed6810ffe6ca062ecb4
Gustavo de Leon <gdeleontobias@gmail.com>
2022-03-06 14:20:38 -0600

    More css changes

--- phoneContact/src/app/app-routing.module.ts
+++ phoneContact/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'people', loadChildren: () => import('./pages/people/people.module').then(m => m.PeopleModule) },
   { path: 'information', loadChildren: () => import('./pages/information/information.module').then(m => m.InformationModule) },
+  { path: '', redirectTo: '/people', pathMatch: 'full'},
   { path: '**', redirectTo: '/people', pathMatch: 'full'}
 ];
 
